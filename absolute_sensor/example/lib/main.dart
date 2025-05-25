16310081220e285c7ebf9954d63318cdb775f095
Abdul Rehman Khilji <abdul@scapia.cards>
2025-05-25 20:32:12 +0530

    Updated example app.

--- absolute_sensor/example/lib/main.dart
+++ absolute_sensor/example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:absolute_sensor/absolute_sensor.dart';
 import 'package:flutter/material.dart';
 
@@ -13,24 +15,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _absoluteSensorPlugin = AbsoluteSensor();
+  String _text = 'Waiting…';
+  late StreamSubscription<SensorData> _accelSub;
 
   @override
   void initState() {
     super.initState();
+    AbsoluteSensor.isSensorAvailable(1).then((ok) {
+      if (ok) {
+        _accelSub = AbsoluteSensor.accelerometerEvents.listen((d) {
+          setState(() {
+            _text = 'x:${d.x.toStringAsFixed(1)}, '
+                'y:${d.y.toStringAsFixed(1)}, '
+                'z:${d.z.toStringAsFixed(1)}';
+          });
+        });
+      } else {
+        setState(() => _text = 'No accelerometer');
+      }
+    });
   }
 
   @override
-  Widget build(BuildContext context) {
+  void dispose() {
+    _accelSub.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext c) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        appBar: AppBar(title: const Text('AbsoluteSensor Demo')),
+        body: Center(child: Text(_text, style: const TextStyle(fontSize: 24))),
       ),
     );
   }
